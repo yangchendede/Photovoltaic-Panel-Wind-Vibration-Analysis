9fa935336fa6b1b6ba3574fdb91b15ddbf16bc12
yangchendede <yangchenhhhh@gmail.com>
2024-03-06 23:58:15 +0800

    model not constrained

--- apdl_calculation_file/intergrateFile.m
+++ apdl_calculation_file/intergrateFile.m
@@ -2,9 +2,13 @@
 fidfina = fopen('intergratedFile.txt', 'w');
 
 % Save file names in order
+% filelist = ["../model/setup.txt", "../model/defineElementTypeConstantMaterial.txt", ...
+%             "../model/geometry.txt", "../force/constraint.txt", ...
+%             "../preAnalysis/zhaoxing.txt", "../modalAnalysis/modalAnalysis.txt"];
+
 filelist = ["../model/setup.txt", "../model/defineElementTypeConstantMaterial.txt", ...
             "../model/geometry.txt", "../force/constraint.txt", ...
-            "../preAnalysis/zhaoxing.txt", "../modalAnalysis/modalAnalysis.txt"];
+            ];
 
 % Read and write file content
 for fileN = 1: numel(filelist)
